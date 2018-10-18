f2dc171377eec1a031cc9a8d913004171318ae58
Rui Ferrao <ruiferraodev@gmail.com>
2018-10-18 20:02:24 +0100

    first layout

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>boxeat</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/mobile.css">
+</head>
+<body>
+	<video autoplay muted loop id="main_video">
+	  <source src="vid/video01.mp4" type="video/mp4">
+	  Your browser does not support HTML5 video.
+	</video>
+	<header>
+		
+	</header>
+	<div class="wrapper">
+
+		<figure><img src="img/boxeat.png"></figure>
+		<div class="content">
+		  <h1>Brevemente!</h1>
+		  <button><span>Encomendar</span></button>
+		</div>
+		
+	</div>
+
+
+</body>
+</html>
